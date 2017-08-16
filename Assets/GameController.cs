804549cf89da7c014035407c8107617c5e9ebd9c
Jacob Blanch <1006027@student.sae.edu.au>
2017-08-16 16:05:47 +1000

    ooohweee

---
+++ Assets/GameController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour {
+
+    public int nazisPunched;
+    public int totalNazis;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (nazisPunched >= 10)
+            Debug.Log("Winner");
+	}
+}
---
+++ Assets/NaziScript.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NaziScript : MonoBehaviour {
+
+    public GameObject retort;
+    public GameObject player;
+    public float force;
+    public Camera m_Camera;
+    bool billboarding = true;
+    bool beenHit;
+    public AudioSource ouchSound;
+    public Rigidbody rb;
+
+
+    // Use this for initialization
+    void Start () {
+        rb = GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+            Application.Quit();
+
+        Debug.Log("Billboarding = " + billboarding);
+        if (Vector3.Distance(transform.position, player.transform.position) <= 8f)
+            if (!retort.activeInHierarchy)
+                if (!beenHit)
+                    retort.SetActive(true);
+        if (Vector3.Distance(transform.position, player.transform.position) <= 2f)
+            rb.isKinematic = false;
+
+        if (billboarding)
+        {
+            transform.LookAt(transform.position + m_Camera.transform.rotation * Vector3.forward,
+            m_Camera.transform.rotation * Vector3.up);
+        }
+        
+    }
+
+    public void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            billboarding = false;
+            beenHit = true;
+            retort.SetActive(false);
+            Vector3 dir = collision.contacts[0].point - transform.position;
+            dir = -dir.normalized;
+            GetComponent<Rigidbody>().AddForce(dir * force);
+            ouchSound.Play();
+        }
+    }
+}
